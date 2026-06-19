3848f26588e3413b71217ca1cdab4274064afe20
timka-lubimka <timateg903@gmail.com>
2026-06-20 01:40:36 +0700

    Добавлен скрипт сборки проекта и плагинов

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+mkdir -p plagins
+
+g++ -std=c++17 -fPIC -shared -o plagins/librc4.so rc4_plugin.cpp
+g++ -std=c++17 -fPIC -shared -o plagins/libvigenere.so vigenere_plugin.cpp
+g++ -std=c++17 -fPIC -shared -o plagins/libelgamal.so elgamal_plugin.cpp
+g++ -std=c++17 -fPIC -shared -o plagins/libxtea.so xtea_plugin.cpp
+g++ -std=c++17 -fPIC -shared -o plagins/libhill.so hill_plugin.cpp
+g++ -std=c++17 -fPIC -shared -o plagins/libotp.so otp_plugin.cpp
+
+g++ -std=c++17 main.cpp file_manager.cpp base64_utils.cpp -ldl -o main
+
+echo "Сборка завершена. Запуск: ./main"
